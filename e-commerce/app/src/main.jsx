f0c4ad9362a7badcbfd593992df72a86796986f9
Habiba Saeed <habiba.saeed033@gmail.com>
2025-05-12 23:02:01 +0300

    Deploy to GitHub Pages

---
+++ e-commerce/app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TrafficAppLanding from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TrafficAppLanding />
+  </React.StrictMode>
+);
